a54b7076e9e2a737b5856bfb75165b74a66054a4
Janosch Woschitz <janosch.woschitz@gmail.com>
2013-05-31 19:53:32 +0200

    fixed whitespace

--- Brutus/Executor.cs
+++ Brutus/Executor.cs
@@ -9,13 +9,13 @@ namespace Brutus
 {
 	public class Executor
 	{   
-	    private char[] keyspace;
-	    
-	    private int indexOfLastCharInKeyspace;
-	    
-	    private char[] lastComputedKey;
+		private char[] keyspace;
 		
-    	public Executor(char[] keyspace)
+		private int indexOfLastCharInKeyspace;
+		
+		private char[] lastComputedKey;
+		
+  		public Executor(char[] keyspace)
 		{
 			if (keyspace.Length == 0) {
 				throw new ArgumentException("Keyspace needs to contain any characters.");
@@ -24,44 +24,44 @@ namespace Brutus
 			// minor performance improvement
 			this.indexOfLastCharInKeyspace = keyspace.Length - 1;
 		}
-    	
-    	public void Reset() {
-    		lastComputedKey = null;
-    	}
 		
-    	public char[] ComputeNextKey() {
-    		if (lastComputedKey == null) {
-    			lastComputedKey = new char[] { keyspace[0] };
-    			return lastComputedKey;
-    		}
-    		
-    		int index = lastComputedKey.Length - 1;
-    		int keyspaceIndex = this.getKeyspaceIndexFor(lastComputedKey[index]);
-    		
-    		while (keyspaceIndex == indexOfLastCharInKeyspace) {
-    			lastComputedKey[index] = keyspace[0];
-    			if (index == 0) {
-    				// preserve the old length as new index
-    				index = lastComputedKey.Length;
-    				Array.Resize<char>(ref lastComputedKey, lastComputedKey.Length + 1); 
-    				lastComputedKey[index] = keyspace[0];
-    				return lastComputedKey;
-    			} else {
-    				index--;
-    				keyspaceIndex = this.getKeyspaceIndexFor(lastComputedKey[index]);
-    			}
-    		}
-    		
-   			lastComputedKey[index] = keyspace[++keyspaceIndex];
-   			return lastComputedKey;
+		public void Reset() {
+			lastComputedKey = null;
 		}
-    	
-    	public char[] getKeyspace() {
-    		return keyspace;
-    	}
-    	
-    	private int getKeyspaceIndexFor(char value) {
-    		return Array.IndexOf(keyspace, value);
-    	}
+		
+		public char[] ComputeNextKey() {
+			if (lastComputedKey == null) {
+				lastComputedKey = new char[] { keyspace[0] };
+				return lastComputedKey;
+			}
+			
+			int index = lastComputedKey.Length - 1;
+			int keyspaceIndex = this.getKeyspaceIndexFor(lastComputedKey[index]);
+			
+			while (keyspaceIndex == indexOfLastCharInKeyspace) {
+				lastComputedKey[index] = keyspace[0];
+				if (index == 0) {
+					// preserve the old length as new index
+					index = lastComputedKey.Length;
+					Array.Resize<char>(ref lastComputedKey, lastComputedKey.Length + 1); 
+					lastComputedKey[index] = keyspace[0];
+					return lastComputedKey;
+				} else {
+					index--;
+					keyspaceIndex = this.getKeyspaceIndexFor(lastComputedKey[index]);
+				}
+			}
+			
+			   lastComputedKey[index] = keyspace[++keyspaceIndex];
+			   return lastComputedKey;
+		}
+		
+		public char[] getKeyspace() {
+			return keyspace;
+		}
+		
+		private int getKeyspaceIndexFor(char value) {
+			return Array.IndexOf(keyspace, value);
+		}
 	}
 }
